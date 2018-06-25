9b31eab42c94e770793f68f24a05542a4fd7cf62
See Loo Jane <muigek34@yahoo.com.sg>
2018-06-25 14:03:24 +0800

    Link up Routes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import Home from "../home/Home";
+import MyCats from "../my-cats/MyCats";
 
 class App extends Component {
 	render() {
 		return (
-			<BrowserRouter>
-				{/* <Route path="/" component={}></Route> */}
-			</BrowserRouter>
+			<Router>
+				<div className="App">
+					<div className="container">
+						<ul>
+							<li>
+								<Link to="/">Home</Link>
+							</li>
+							<li>
+								<Link to="/my-cats">My Cats</Link>
+							</li>
+						</ul>
+						<hr />
+						<Route exact={true} path="/" component={Home} />
+						<Route
+							exact={true}
+							path="/my-cats"
+							component={MyCats}
+						/>
+					</div>
+				</div>
+			</Router>
 		);
 	}
 }
