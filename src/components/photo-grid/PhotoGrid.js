e219ca7279b411fffdfd44b776daf8e1ac4c9381
See Loo Jane <muigek34@yahoo.com.sg>
2018-06-25 15:50:21 +0800

    Link up PhotoGrid and PhotoGridItem, and it will only show when items are loaded (prevents undefined.map)

--- src/components/photo-grid/PhotoGrid.js
+++ src/components/photo-grid/PhotoGrid.js
@@ -1,29 +1,18 @@
 import React, { Component } from "react";
-import { Row, Grid, Col, Thumbnail, Button, Image } from "react-bootstrap";
+import { Row, Grid } from "react-bootstrap";
 
 import "./PhotoGrid.css";
+import PhotoGridItem from "../photo-grid-item/PhotoGridItem";
 
 class PhotoGrid extends Component {
 	render() {
-		let gridItem = (
-			<Col xs={12} md={6} lg={3}>
-				<Thumbnail src="http://25.media.tumblr.com/tumblr_lpetx6Bl2q1qmvo0go1_500.jpg">
-					{/* <p>
-						<Button bsStyle="primary">Button</Button>&nbsp;
-						<Button bsStyle="default">Button</Button>
-					</p> */}
-				</Thumbnail>
-			</Col>
-		);
-
 		return (
 			<div>
 				<Grid className="container-fluid">
 					<Row>
-						{gridItem}
-						{gridItem}
-						{gridItem}
-						{gridItem}
+						{this.props.hasLoaded && this.props.cats.map(pic => {
+							return <PhotoGridItem src={pic} />;
+						})}
 					</Row>
 				</Grid>
 			</div>
