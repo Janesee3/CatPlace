484d400828afff46331d11c435809ef7b5b9af16
See Loo Jane <muigek34@yahoo.com.sg>
2018-06-25 15:35:52 +0800

    Basic styling for thumbnail images in Grid

--- src/components/photo-grid/PhotoGrid.js
+++ src/components/photo-grid/PhotoGrid.js
@@ -1,21 +1,29 @@
 import React, { Component } from "react";
-import { Row, Grid, Col } from "react-bootstrap";
+import { Row, Grid, Col, Thumbnail, Button, Image } from "react-bootstrap";
 
 import "./PhotoGrid.css";
 
 class PhotoGrid extends Component {
 	render() {
+		let gridItem = (
+			<Col xs={12} md={6} lg={3}>
+				<Thumbnail src="http://25.media.tumblr.com/tumblr_lpetx6Bl2q1qmvo0go1_500.jpg">
+					{/* <p>
+						<Button bsStyle="primary">Button</Button>&nbsp;
+						<Button bsStyle="default">Button</Button>
+					</p> */}
+				</Thumbnail>
+			</Col>
+		);
+
 		return (
 			<div>
-				<div>My Cats</div>
-				<Grid>
-					<Row className="show-grid">
-						<Col xs={12} md={8}>
-							First
-						</Col>
-						<Col xs={6} md={4}>
-							Second
-						</Col>
+				<Grid className="container-fluid">
+					<Row>
+						{gridItem}
+						{gridItem}
+						{gridItem}
+						{gridItem}
 					</Row>
 				</Grid>
 			</div>
