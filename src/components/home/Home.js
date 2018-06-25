74a909f3b8d73125d7d1a60e88c8fa403a2adc66
See Loo Jane <muigek34@yahoo.com.sg>
2018-06-25 14:31:04 +0800

    Link up api call with state

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,6 +3,48 @@ import React, { Component } from "react";
 import "./Home.css";
 
 class Home extends Component {
+	constructor() {
+		super();
+		this.state = {
+			cats: []
+		};
+	}
+
+	componentDidMount() {
+		this.getCats();
+	}
+
+	getCats() {
+		fetch(
+			"http://thecatapi.com/api/images/get?format=html&results_per_page=3"
+		)
+			.then(res => {
+				return res.text();
+			})
+			.then(text => {
+				this.setState({ cats: this.extractImgUrl(text) });
+				console.log(this.state.cats);
+			});
+	}
+
+	// Returns an array image urls from the input htmlText string
+	extractImgUrl(htmlText) {
+		let regex = new RegExp('<img src="(.+)"', "g");
+		let matches = this.getMatches(htmlText, regex, 1);
+		return matches;
+	}
+
+	// Returns an array of matches for the input group index
+	getMatches(string, regex, index) {
+		index || (index = 1); // default to the first capturing group
+		var matches = [];
+		var match;
+		while ((match = regex.exec(string))) {
+			matches.push(match[index]);
+		}
+		return matches;
+	}
+
 	render() {
 		return <div>Home</div>;
 	}
