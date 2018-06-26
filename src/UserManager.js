ef892d968be407b0eb59628dbd5a22b8bd8cc631
See Loo Jane <muigek34@yahoo.com.sg>
2018-06-26 11:09:55 +0800

    rework UserManager

--- src/UserManager.js
+++ src/UserManager.js
@@ -3,32 +3,40 @@ import User from "./User";
 class UserManager {
 	constructor() {}
 
-	// getUserId() {
-	// 	let userId = localStorage.getItem("userId");
-	// 	if (userId == null) {
-	// 		userId = JSON.stringify(new Date());
-	// 		localStorage.setItem("userId", userId);
-	// 	}
-	// 	// Reaccess value because of possible changes to the value inside if condition
-	// 	let newId = localStorage.getItem("userId");
-	// 	console.log(userId);
-	// 	return newId;
-	// }
+	getUserId() {
+		let userId = localStorage.getItem("userId");
+		if (userId == null || userId == "null") {
+			userId = JSON.stringify(new Date());
+			localStorage.setItem("userId", userId);
+		}
+		// Reaccess value because of possible changes to the value inside if condition
+		let newId = localStorage.getItem("userId");
+		console.log(userId);
+		return newId;
+	}
 
-	// removeUserId() {
-	// 	localStorage.setItem("userId", null);
-	// }
+	removeUserId() {
+		localStorage.setItem("userId", null);
+	}
 
 	// Returns a User object stored in the localStorage.
 	// If no User object exists, method will create a new one and return it.
 	getUser() {
 		localStorage.setItem("user", null);
 		let user = localStorage.getItem("user");
-		if (user === "null") {
+		if (user == null) {
 			user = new User(null, null);
 			localStorage.setItem("user", this.userToString(user));
 		}
 		return this.stringToUser(localStorage.getItem("user"));
+	}
+
+	updateFavourites(newId) {
+		let user = this.getUser();
+		let faves = user.getFavourites();
+		faves.push(newId);
+		user.setFavourites(faves);
+		localStorage.setItem("user", this.userToString(user));
 	}
 
 	/** Serialize/Deserialize **/
@@ -39,7 +47,7 @@ class UserManager {
 	}
 
 	userToString(user) {
-		return JSON.stringify(user);
+		return JSON.parse(user);
 	}
 }
 
