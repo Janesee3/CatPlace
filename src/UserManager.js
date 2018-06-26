9d6663f9ebde37a4e02393c4a41d6aae25d91f1b
See Loo Jane <muigek34@yahoo.com.sg>
2018-06-26 11:00:17 +0800

    Add methods to serialise / deserialzie User obj and save/load from local storage

--- src/UserManager.js
+++ src/UserManager.js
@@ -1,19 +1,46 @@
+import User from "./User";
+
 class UserManager {
 	constructor() {}
 
-	getUserId() {
-		let userId = localStorage.getItem("userId");
-		if (userId == null) {
-			userId = JSON.stringify(new Date());
-			localStorage.setItem("userId", userId);
+	// getUserId() {
+	// 	let userId = localStorage.getItem("userId");
+	// 	if (userId == null) {
+	// 		userId = JSON.stringify(new Date());
+	// 		localStorage.setItem("userId", userId);
+	// 	}
+	// 	// Reaccess value because of possible changes to the value inside if condition
+	// 	let newId = localStorage.getItem("userId");
+	// 	console.log(userId);
+	// 	return newId;
+	// }
+
+	// removeUserId() {
+	// 	localStorage.setItem("userId", null);
+	// }
+
+	// Returns a User object stored in the localStorage.
+	// If no User object exists, method will create a new one and return it.
+	getUser() {
+		localStorage.setItem("user", null);
+		let user = localStorage.getItem("user");
+		if (user === "null") {
+			user = new User(null, null);
+			localStorage.setItem("user", this.userToString(user));
 		}
-		// Reaccess value because of possible changes to the value inside if condition
-		let newId = localStorage.getItem("userId");
-		console.log(userId);
-		return newId;
+		return this.stringToUser(localStorage.getItem("user"));
 	}
 
-	forgetUserId() {
-		localStorage.setItem("userId", null);
+	/** Serialize/Deserialize **/
+
+	stringToUser(string) {
+		let obj = JSON.parse(string);
+		return new User(obj["id"], obj["favourites"]);
+	}
+
+	userToString(user) {
+		return JSON.stringify(user);
 	}
 }
+
+export default new UserManager();
