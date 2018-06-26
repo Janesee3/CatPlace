2283b0800e4b3dcd9ac8fa3fa688472b2d838e76
See Loo Jane <muigek34@yahoo.com.sg>
2018-06-26 11:15:22 +0800

    Comment out unnecessary codes

--- src/User.js
+++ src/User.js
@@ -1,31 +1,31 @@
-class User {
-	// Creates a User object.
-	// If initialising a new User with default params, input (null, null);
-	constructor(id, favourites) {
-		if (id == null) {
-			this.id = JSON.stringify(new Date());
-		} else {
-			this.id = id;
-		}
+// class User {
+// 	// Creates a User object.
+// 	// If initialising a new User with default params, input (null, null);
+// 	constructor(id, favourites) {
+// 		if (id == null) {
+// 			this.id = JSON.stringify(new Date());
+// 		} else {
+// 			this.id = id;
+// 		}
 
-		if (favourites == null) {
-			this.favourites = [];
-		} else {
-			this.favourites = favourites;
-		}
-	}
+// 		if (favourites == null) {
+// 			this.favourites = [];
+// 		} else {
+// 			this.favourites = favourites;
+// 		}
+// 	}
 
-	getId() {
-		return this.id;
-	}
+// 	getId() {
+// 		return this.id;
+// 	}
 
-	getFavourites() {
-		return this.favourites;
-	}
+// 	getFavourites() {
+// 		return this.favourites;
+// 	}
 
-	setFavourites(faves) {
-		this.favourites = faves;
-	}
-}
+// 	setFavourites(faves) {
+// 		this.favourites = faves;
+// 	}
+// }
 
-export default User;
+// export default User;
--- src/UserManager.js
+++ src/UserManager.js
@@ -19,36 +19,36 @@ class UserManager {
 		localStorage.setItem("userId", null);
 	}
 
-	// Returns a User object stored in the localStorage.
-	// If no User object exists, method will create a new one and return it.
-	getUser() {
-		localStorage.setItem("user", null);
-		let user = localStorage.getItem("user");
-		if (user == null) {
-			user = new User(null, null);
-			localStorage.setItem("user", this.userToString(user));
-		}
-		return this.stringToUser(localStorage.getItem("user"));
-	}
+	// // Returns a User object stored in the localStorage.
+	// // If no User object exists, method will create a new one and return it.
+	// getUser() {
+	// 	localStorage.setItem("user", null);
+	// 	let user = localStorage.getItem("user");
+	// 	if (user == null) {
+	// 		user = new User(null, null);
+	// 		localStorage.setItem("user", this.userToString(user));
+	// 	}
+	// 	return this.stringToUser(localStorage.getItem("user"));
+	// }
 
-	updateFavourites(newId) {
-		let user = this.getUser();
-		let faves = user.getFavourites();
-		faves.push(newId);
-		user.setFavourites(faves);
-		localStorage.setItem("user", this.userToString(user));
-	}
+	// updateFavourites(newId) {
+	// 	let user = this.getUser();
+	// 	let faves = user.getFavourites();
+	// 	faves.push(newId);
+	// 	user.setFavourites(faves);
+	// 	localStorage.setItem("user", this.userToString(user));
+	// }
 
-	/** Serialize/Deserialize **/
+	// /** Serialize/Deserialize **/
 
-	stringToUser(string) {
-		let obj = JSON.parse(string);
-		return new User(obj["id"], obj["favourites"]);
-	}
+	// stringToUser(string) {
+	// 	let obj = JSON.parse(string);
+	// 	return new User(obj["id"], obj["favourites"]);
+	// }
 
-	userToString(user) {
-		return JSON.parse(user);
-	}
+	// userToString(user) {
+	// 	return JSON.parse(user);
+	// }
 }
 
 export default new UserManager();
