a1fb295694b08aa60b280366172c1e725b04807d
See Loo Jane <muigek34@yahoo.com.sg>
2018-06-26 09:54:46 +0800

    Create GlobalVar class file to store api key (not sure if this is good for performance overhead?)

---
+++ src/GlobalVars.js
@@ -0,0 +1,7 @@
+class GlobalVars {
+	constructor() {
+		this.API_KEY = "MzMxNjI4";
+	}
+}
+
+export default new GlobalVars();
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,8 +4,13 @@ import "./App.css";
 import Home from "../home/Home";
 import MyCats from "../my-cats/MyCats";
 import Header from "../header/Header";
+// import GlobalVars from "../../GlobalVars";
 
 class App extends Component {
+	componentDidMount() {
+		// console.log(GlobalVars.API_KEY);
+	}
+
 	render() {
 		return (
 			<Router>
