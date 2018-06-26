225c723a474f344f2841d9b4470e81dbe89b4501
See Loo Jane <muigek34@yahoo.com.sg>
2018-06-26 11:31:53 +0800

    Add grid type prop to PhotoGrid to customise views for different uses

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,6 +3,7 @@ import "./Home.css";
 import PhotoGrid from "../photo-grid/PhotoGrid";
 import UserManager from "../../UserManager";
 import GlobalVars from "../../GlobalVars";
+import Utility from "../../Utility";
 
 class Home extends Component {
 	constructor() {
@@ -31,6 +32,7 @@ class Home extends Component {
 				<PhotoGrid
 					cats={this.state.cats}
 					hasLoaded={!this.state.isLoading}
+					type={GlobalVars.GRID_TYPE.HOME}
 					handleClickSave={this.handleClickSave}
 				/>
 			</div>
@@ -46,7 +48,7 @@ class Home extends Component {
 			})
 			.then(text => {
 				this.setState({
-					cats: this.extractImgObjects(text),
+					cats: Utility.extractImgObjects(text),
 					isLoading: false
 				});
 
@@ -69,30 +71,6 @@ class Home extends Component {
 				console.log("favourited!");
 			})
 			.catch(err => console.log(err));
-	}
-
-	// *****  Utility Methods ***** //
-
-	// Returns an array {key, url (image)} from the input text string
-	extractImgObjects(text) {
-		let regexUrl = new RegExp("<url>(.+)</url>", "g");
-		let regexId = new RegExp("<id>(.+)</id>", "g");
-		let urls = this.getMatches(text, regexUrl, 1);
-		let ids = this.getMatches(text, regexId, 1);
-		return urls.map((url, index) => {
-			return { key: ids[index], src: url };
-		});
-	}
-
-	// Returns an array of matches for the input group index
-	getMatches(string, regex, index) {
-		index || (index = 1); // default to the first capturing group
-		var matches = [];
-		var match;
-		while ((match = regex.exec(string))) {
-			matches.push(match[index]);
-		}
-		return matches;
 	}
 }
 
