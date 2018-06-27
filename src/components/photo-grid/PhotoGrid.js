b4548bf631d7f2818acd01b419bb6367a8b72186
See Loo Jane <muigek34@yahoo.com.sg>
2018-06-27 14:40:20 +0800

    Refactor PhotoGrid into stateless component

--- src/components/photo-grid/PhotoGrid.js
+++ src/components/photo-grid/PhotoGrid.js
@@ -3,26 +3,22 @@ import { Row, Grid } from "react-bootstrap";
 import "./PhotoGrid.css";
 import PhotoGridItem from "../photo-grid-item/PhotoGridItem";
 
-class PhotoGrid extends Component {
-	render() {
-		return (
-			<Grid className="container-fluid">
-				<Row>
-					{this.props.hasLoaded &&
-						this.props.cats.map(cat => {
-							return (
-								<PhotoGridItem
-									key={cat.key}
-									id={cat.key}
-									src={cat.src}
-									callback={this.props.callback}
-								/>
-							);
-						})}
-				</Row>
-			</Grid>
-		);
-	}
-}
+const PhotoGrid = (props) => (
+	<Grid className="container-fluid">
+		<Row>
+			{props.hasLoaded &&
+				props.cats.map(cat => {
+					return (
+						<PhotoGridItem
+							key={cat.key}
+							id={cat.key}
+							src={cat.src}
+							callback={props.callback}
+						/>
+					);
+				})}
+		</Row>
+	</Grid>
+);
 
 export default PhotoGrid;
