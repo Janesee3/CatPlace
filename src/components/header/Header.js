2abc93addf407ca79b7fbef4cc4928714f6fa9f8
See Loo Jane <muigek34@yahoo.com.sg>
2018-06-27 13:38:00 +0800

    Styling for header + home

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Image } from "react-bootstrap";
 import "./Header.css";
 import NavBar from "../nav-bar/NavBar";
 import icon from "../../assets/kitten_icon.png";
@@ -8,9 +7,9 @@ class Header extends Component {
 	render() {
 		return (
 			<div className="header">
-				<img src={icon} />
-				<h1 className="heading">The Cat Place</h1>
-				<h2 className="subheading">Get your daily dose of Cat here!</h2>
+				<img src={icon} alt="kitten-icon" />
+				<h1 className="title">The Cat Place</h1>
+				<h2 className="subtitle">Get your daily dose of Cat here!</h2>
 				<NavBar />
 			</div>
 		);
