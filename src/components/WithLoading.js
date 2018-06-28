2a38dc4f60da8dd46c4ae305cd96f9e26ad65800
See Loo Jane <muigek34@yahoo.com.sg>
2018-06-29 00:18:33 +0800

    Create withLoading HOC

--- src/components/WithLoading.js
+++ src/components/WithLoading.js
@@ -1,19 +1,10 @@
 import React from "react";
-import Utility from '../Utility'; 
 
-export const withLoading = Component =>
-
-	class withLoading extends Component {
-		render() {
-			return ({ isLoading, ...props }) => {
-                if (!isLoading) {
-                  return <Component { ...props } />;
-                }
-            ​
-                return <LoadingSpinner />;
-              };
-		}
-    };
-
-    // const LoadingSpinner = () => return <div>Loading...</div>; 
-    // TODO: FIX THIS!
+export const withLoadingIndicator = Component => props =>
+	props.isLoading ? (
+		<div>
+			<p>Loading Cats ...</p>
+		</div>
+	) : (
+		<Component {...props} />
+	);
