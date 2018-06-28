b17e0f751883e4b93ac56cfe2f01ca05fde03899
See Loo Jane <muigek34@yahoo.com.sg>
2018-06-28 14:32:27 +0800

    Remove unused import

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Header from "../header/Header";
 import NavRoutes from "../NavRoutes";
