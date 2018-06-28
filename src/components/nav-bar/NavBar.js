76ed4c5a6e2c15018a6c1504500becc0ce28ef30
See Loo Jane <muigek34@yahoo.com.sg>
2018-06-28 12:33:59 +0800

    Refactor NavBar into dummy component

--- src/components/nav-bar/NavBar.js
+++ src/components/nav-bar/NavBar.js
@@ -1,26 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { Row, Col } from "react-bootstrap";
-
 import "./NavBar.css";
 
-class NavBar extends Component {
-	render() {
-		return (
-			<div className="nav-bar">
-				<Row>
-					<Col xs={12} md={6}>
-						<Link to="/">
-							<span>Home</span>
-						</Link>
-					</Col>
-					<Col xs={12} md={6}>
-						<Link to="/my-cats">My Cats</Link>
-					</Col>
-				</Row>
-			</div>
-		);
-	}
-}
+const NavBar = () => (
+	<div className="nav-bar">
+		<Row>
+			<Col xs={12} md={6}>
+				<Link to="/">
+					<span>Home</span>
+				</Link>
+			</Col>
+			<Col xs={12} md={6}>
+				<Link to="/my-cats">My Cats</Link>
+			</Col>
+		</Row>
+	</div>
+);
 
 export default NavBar;
